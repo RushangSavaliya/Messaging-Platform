cba742f12c0fbbb2e37ce6206b94f4d738e9dfa3
Rushang Savaliya <rushangsavaliya19@gmail.com>
2025-06-02 16:22:55 +0530

    Add CORS middelware

--- middlewares/global.middleware.js
+++ middlewares/global.middleware.js
@@ -1,10 +1,15 @@
 // middlewares/global.middleware.js
 
 import express from 'express';
+import cors from 'cors';
+// ──────────────────────────────
+// 1. GLOBAL MIDDLEWARES
+// ──────────────────────────────
 
 const setupMiddlewares = (app) => {
     app.use(express.json());
-    // Add more global middlewares here (CORS, morgan, etc.)
+    app.use(cors());  // Enable CORS for all routes
+    // Add more global middlewares here 
 };
 
 export default setupMiddlewares;
