49c11a53081ebc67d9317f357ecfb810f2958d1d
Rushang Savaliya <rushangsavaliya19@gmail.com>
2025-06-03 11:35:00 +0530

    Add login api

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -5,6 +5,7 @@ import User from '../models/User.js';
 
 const router = express.Router();
 
+// Register route
 router.post('/register', async (req, res) => {
     const { username, email, password } = req.body;
 
@@ -32,6 +33,34 @@ router.post('/register', async (req, res) => {
             });
         }
 
+        return res.status(500).json({
+            error: error.message,
+        });
+    }
+});
+
+// Login route
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    // Validate required fields
+    if (!username || !password) {
+        return res.status(400).json({
+            error: 'Username and password are required',
+        });
+    }
+
+    try {
+        const user = await User.findOne({ username });
+        if (!user || user.password !== password) {
+            return res.status(401).json({
+                error: 'Invalid username or password',
+            });
+        }
+        return res.status(200).json({
+            message: 'Login successful',
+        });
+    } catch (error) {
         return res.status(500).json({
             error: error.message,
         });
